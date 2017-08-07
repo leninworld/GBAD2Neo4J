8d2b46fde12e5fda792c0a9d96ad064f33fc6597
lenin <lenin.world@gmail.com>
2017-08-06 21:44:07 -0500

    first commit

---
+++ src/main/scala/Convert_GBAD_2_Neo3J_Cypher.scala
@@ -0,0 +1,40 @@
+
+import scala.io.Source
+import java.io.{FileNotFoundException, IOException}
+
+// CLASS
+//class Convert_GBAD_2_Neo3J_Cypher {
+//  def helloWorld() { println("hey") }
+//}
+
+// OBJECT
+object Convert_GBAD_2_Neo3J_Cypher extends App  {
+
+  // convert GBAD to Neo4J
+  def Convert_GBAD_2_Neo3J_Cypherfn(inFile:String): Unit = {
+    try{
+
+      println("inFile:"+inFile);
+
+      for (line <- Source.fromFile(inFile).getLines) {
+          println(line)
+
+          var arr = line.split("\\s+")
+
+           println("atom. "+arr.length +" "+arr(0) + " "+arr(1)+" "+arr(2))
+
+      }
+
+    } catch{
+      case ex: IllegalArgumentException => print(ex+  ex.getMessage)
+    }
+  }
+
+  val baseFolder = "/Users/lenin/Dropbox/#problems/p19/C995_only/"
+  var filename = "C995_OUTPUT_3.g"
+      filename = baseFolder + filename
+
+  // convert GBAD to Neo4L
+  Convert_GBAD_2_Neo3J_Cypherfn(filename);
+
+}
